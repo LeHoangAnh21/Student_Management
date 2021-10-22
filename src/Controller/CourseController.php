8acb71eb75a31e1f4f99abc7f4891cc8957bce23
LeHoangAnh21 <84314151+LeHoangAnh21@users.noreply.github.com>
2021-10-22 21:28:00 +0700

    ok

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,19 +2,122 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Entity\Course;
+use App\Form\CourseType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CourseController extends AbstractController
 {
     /**
-     * @Route("/course", name="course")
+     * @Route("/course", name="course_index")
      */
-    public function index(): Response
+    public function courseIndex()
     {
-        return $this->render('course/index.html.twig', [
-            'controller_name' => 'CourseController',
-        ]);
+        $courses = $this->getDoctrine()->getRepository(Course::class)->findAll();
+
+        return $this->render(
+            'course/index.html.twig', 
+            [
+                'courses' => $courses
+            ]
+        );
+    }
+
+    /**
+     * @Route("/course/detail/{id}", name="course_detail")
+     */
+    public function courseDetail($id)
+    {
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($id);
+
+        if($course == null){
+            $this->addFlash('Error', 'Course not found!');
+            return $this->redirectToRoute('course_index');
+        }
+        else{
+            return $this->render(
+                'course/detail.html.twig', 
+                [
+                    'course' => $course
+                ]
+            );
+        }
+    }
+
+    /**
+     * @Route("/course/delete/{id}", name="course_delete")
+     */
+    public function deleteCourse($id)
+    {
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($id);
+
+        if($course == null){
+            $this->addFlash('Error', 'Course not found!');
+        }
+        else{
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($course);
+            $manager->flush();
+            $this->addFlash('Success', 'Course has been deleted!');
+        }
+        return $this->redirectToRoute('course_index');
+    }
+
+    /**
+     * @Route("/course/add", name="course_add")
+     */
+    public function addCourse(Request $request)
+    {
+        $course = new Course();
+        $form = $this->createForm(CourseType::class, $course);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($course);
+            $manager->flush();
+
+            $this->addFlash('Success', "Add course successfully !");
+            return $this->redirectToRoute("course_index");
+        }
+
+        return $this->render(
+            "course/add.html.twig",
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("course/edit/{id}", name="course_edit")
+     */
+    public function editCourse(Request $request, $id)
+    {
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($id);
+        $form = $this->createForm(CourseType::class, $course);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($course);
+            $manager->flush();
+
+            $this->addFlash('Success', "Course has been updated successfully !");
+            return $this->redirectToRoute("course_index");
+        }
+
+        return $this->render (
+            "course/edit.html.twig", 
+            [
+                'form' => $form->createView()
+            ]
+        );
     }
 }
--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,19 +2,165 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Teacher;
+use App\Form\TeacherType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class TeacherController extends AbstractController
 {
     /**
-     * @Route("/teacher", name="teacher")
+     * @Route("/teacher", name="teacher_index")
      */
-    public function index(): Response
+    public function teacherIndex()
     {
-        return $this->render('teacher/index.html.twig', [
-            'controller_name' => 'TeacherController',
-        ]);
+        $teachers = $this->getDoctrine()->getRepository(Teacher::class)->findAll();
+
+        return $this->render(
+            'teacher/index.html.twig', 
+            [
+                'teachers' => $teachers
+            ]
+        );
+    }
+
+    /**
+     * @Route("/teacher/detail/{id}", name="teacher_detail")
+     */
+    public function teacherDetail($id)
+    {
+        $teacher = $this->getDoctrine()->getRepository(Teacher::class)->find($id);
+
+        if($teacher == null){
+            $this->addFlash('Error', 'Teacher not found!');
+            return $this->redirectToRoute('teacher_index');
+        }
+        else{
+            return $this->render(
+                'teacher/detail.html.twig', 
+                [
+                    'teacher' => $teacher
+                ]
+            );
+        }
+    }
+
+    /**
+     * @Route("/teacher/delete/{id}", name="teacher_delete")
+     */
+    public function deleteTeacher($id)
+    {
+        $teacher = $this->getDoctrine()->getRepository(Teacher::class)->find($id);
+
+        if($teacher == null){
+            $this->addFlash('Error', 'Teacher not found!');
+        }
+        else{
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($teacher);
+            $manager->flush();
+            $this->addFlash('Success', 'Teacher has been deleted!');
+        }
+        return $this->redirectToRoute('teacher_index');
+    }
+
+    /**
+     * @Route("/teacher/add", name="teacher_add")
+     */
+    public function addTeacher(Request $request)
+    {
+        $teacher = new Teacher();
+        $form = $this->createForm(TeacherType::class, $teacher);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $image = $teacher->getImage();
+            $imgName = uniqid();
+            $imgExtension = $image->guessExtension();
+            $imageName = $imgName . "." . $imgExtension;
+
+            try{
+                $image->move(
+                    $this->getParameter('teacher_image'), $imageName
+                );
+            }
+            catch(FileException $e){
+
+            }
+            $teacher->setImage($imageName);
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($teacher);
+            $manager->flush();
+
+            $this->addFlash('Success', "Add teacher successfully !");
+            return $this->redirectToRoute("teacher_index");
+        }
+
+        return $this->render(
+            "teacher/add.html.twig",
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("teacher/edit/{id}", name="teacher_edit")
+     */
+    public function editTeacher(Request $request, $id)
+    {
+        $teacher = $this->getDoctrine()->getRepository(Teacher::class)->find($id);
+        
+        if ($teacher == null) 
+        {
+            $this->addFlash('Error', 'Teacher not found !');
+            return $this->redirectToRoute('teacher_index');
+        } 
+        else 
+        { 
+            $form = $this->createForm(TeacherType::class, $teacher);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) 
+            {
+                $file = $form['image']->getData();
+
+                if ($file != null)
+                {
+                    $image = $teacher->getImage();
+                    $imgName = uniqid();
+                    $imgExtension = $image->guessExtension();
+                    $imageName = $imgName . "." . $imgExtension;
+
+                    try {
+                        $image->move(
+                            $this->getParameter('teacher_image'), $imageName
+                        );
+                    } 
+                    catch (FileException $e) {
+
+                    }
+
+                    $teacher->setImage($imageName);
+                }
+                    $manager = $this->getDoctrine()->getManager();
+                    $manager->persist($teacher);
+                    $manager->flush();
+
+                    $this->addFlash('Success', "Update teacher successfully !");
+                    return $this->redirectToRoute("teacher_index");
+            }
+
+            return $this->render(
+                "teacher/edit.html.twig",
+                [
+                    'form' => $form->createView()
+                ]
+            );
+        }
     }
 }
