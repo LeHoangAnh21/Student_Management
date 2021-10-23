c138e96f0485b2d6a7d4df58cb26a9d1f1516199
Yonkoulong <nguyenhailong270921@gmail.com>
2021-10-23 14:14:34 +0700

    delete classroom

--- src/Controller/ClassroomController.php
+++ src/Controller/ClassroomController.php
@@ -15,30 +15,50 @@ class ClassroomController extends AbstractController
     public function classroomIndex()
     {
         $classrooms = $this->getDoctrine()->getRepository(Classroom::class)->findAll();
-        if($classrooms == null) {
-            $this->addFlash('Error', 'Class list is empty');
-        }
         return $this->render(
             'classroom/index.html.twig', 
-        [
+            [
             'classrooms' => $classrooms
-        ]);
+            ]
+        );
     }
 
     /**
-     * @Route("/classroom/detail/{id}, name="classroom_detail")
+     * @Route("/classroom/detail/{id}", name="classroom_detail")
      */
     public function classroomDetail($id)
     {
-        $classroom = $this->getDoctrine()->getRepository(Classroom::class)->find($id);
-        if($classroom == null) {
-            $this->addFlash('Error', 'Class is not found!');
-        } else {
+        $classroom = $this->getDoctrine()->getRepository(classroom::class)->find($id);
+        if ($classroom == null) {
+            $this->addFlash('Error', 'classroom not found');
+            return $this->redirectToRoute('classroom_index');
+        } 
+        else {
             return $this->render(
                 'classroom/detail.html.twig',
-            [
-                    'classroom' => $classroom,
-            ]);
+                [
+                    'classroom' => $classroom
+                ]
+            );
         }
     }
+
+    /**
+     * @Route("/classroom/delete/{id}", name="classroom_delete")
+     */
+    public function classroomDelete($id)
+    {
+        $classroom = $this->getDoctrine()->getRepository(Classroom::class)->find($id);
+        if ($classroom == null) {
+            $this->addFlash('Error', 'Class not found');
+        } else {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($classroom);
+            $manager->flush();
+            $this->addFlash('Success', 'Class is deleted');
+        }
+        return $this->redirectToRoute('classroom_index');
+    }
+
+    
 }
