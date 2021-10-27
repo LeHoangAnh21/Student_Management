e1ecca99d7e219ae5710c88b04dfe39fee3de6f2
hoangduongngocthao <thaohdngch190302@fpt.edu.vn>
2021-10-27 11:28:27 +0700

    oke

--- migrations/Version20211026142638.php
+++ migrations/Version20211026142638.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211024023646 extends AbstractMigration
+final class Version20211026142638 extends AbstractMigration
 {
     public function getDescription(): string
     {
